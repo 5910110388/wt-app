b3e446f285670fdf7931d31f8772f05766ea48a1
Atthavit <5910110388@psu.ac.th>
2018-09-24 11:56:39 +0700

    add flexbox layout

--- components/Weather.js
+++ components/Weather.js
@@ -23,15 +23,28 @@ export default class Weather extends React.Component {
         return(
             <View style={styles.container}>
                 <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+                <View style={styles.layout}>
                     <Text>Zip code is {this.props.zipCode}.</Text>
                     <Forcast {...this.state.forcast} />
+                </View>
                 </ImageBackground>
             </View>
+            
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: { paddingTop: 25},
+  container: { paddingTop: 25
+
+  },
   backdrop: {width: '100%', height: '100%'},
+
+  layout:{
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    height: '50%',
+  }
 });
