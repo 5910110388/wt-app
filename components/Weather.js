eef4d7808a3c64df56f1f5edd34865999e8eb499
Atthavit <5910110388@psu.ac.th>
2018-09-24 10:25:57 +0700

    add Weather.js

---
+++ components/Weather.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Weather from './components/weather'
+
+export default class Weather extends React.Component {
+
+  render() {
+    return (
+        <Weather/>
+    );
+  }
+}
+
+
