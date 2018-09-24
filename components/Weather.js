88eebb13ab516986f0aad97559ddc131c532533c
Atthavit <5910110388@psu.ac.th>
2018-09-24 11:21:49 +0700

    apply background to application

--- components/Weather.js
+++ components/Weather.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import Weather from './components/weather'
+import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground } from 'react-native';
+import Forcast from './Forcast';
+
+
+
 
 export default class Weather extends React.Component {
-
-  render() {
-    return (
-        <Weather/>
+  doIt = () => {
+    console.log("Hello from console")
+  }
+  
+    constructor(props){
+        super(props);
+        this.state = {
+            forcast:{
+                main: '-', description: '-', temp: 0
+            }
+        }
+    }
+    render() {
+        return(
+            <View style={styles.container}>
+                <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+                    <Text>Zip code is {this.props.zipCode}.</Text>
+                    <Forcast {...this.state.forcast} />
+                </ImageBackground>
+            </View>
     );
   }
 }
 
-
+const styles = StyleSheet.create({
+  container: { paddingTop: 25},
+  backdrop: {width: '100%', height: '100%'},
+});
